bfad9e36a18c9ab99dee4a19a78b71cdfa6bc5fd
nathan kim <nathanckim18@gmail.com>
2023-08-13 14:27:44 -0400

    data, good enough for now

--- data/get_googlemap.R
+++ data/get_googlemap.R
@@ -2,14 +2,21 @@ library(httr)
 library(tidyverse)
 library(sf)
 library(tigris)
-search_googlemap <- function(point, radius){
+
+config <- list(
+  "Detroit" = st_read("static/shapefiles/detroit") |> select(),
+  "Chicago" = st_read("static/shapefiles/chicago") |>
+    select() |> 
+    st_transform(st_crs(detroit))
+)
+
+search_googlemap <- function(point, query, radius){
   Sys.sleep(0.1)
-  response <- GET("https://maps.googleapis.com", path = "/maps/api/place/findplacefromtext/json",
+  response <- GET("https://maps.googleapis.com", path = "/maps/api/place/textsearch/json",
       query = list(
-        input = "restaurants",
-        inputtype = "textquery",
-        locationbias = paste0("circle:", radius, "@", point[2], ",", point[1]),
-        fields = "formatted_address,name,geometry",
+        input = query,
+        radius = radius,
+        location = paste0(point[2], ",", point[1]),
         key = Sys.getenv("MAPS_KEY")
       )
   ) |> 
@@ -18,32 +25,75 @@ search_googlemap <- function(point, radius){
   if(response$status == "ZERO_RESULTS"){
     return(tibble(querypoint = point))
   }
-  
+
   # and otherwise extract the correct response
-  map_dfr(response$candidates, \(item){
+  map_dfr(response$results, \(item){
     return(tibble(
-      address = item$formattted_address,
+      address = item$formatted_address,
       lat = item$geometry$location$lat,
       lng = item$geometry$location$lng,
       name = item$name,
-      query_lng = point[1],
-      query_lat = point[2],
+      rating = item$rating,
+      n_ratings = item$user_ratings_total
     ))
   })
 }
 
-# Wraps the above
-get_googlemap <- function(){
-  n <- 50
-  detroit <- metro_divisions(2020) |> 
-    filter(str_detect(NAMELSAD, "Detroit")) |> 
+get_geography_restaurants <- function(shape, city, query, n){
+  shape <- shape |> 
     st_make_grid(n = n, what = "centers")
-  detroit_meters <- detroit |> st_transform(26918) 
-  cellsize <- c(diff(st_bbox(detroit_meters)[c(1, 3)]),
-                diff(st_bbox(detroit_meters)[c(2, 4)]))/n
+  shape_meters <- shape |> st_transform(26918) 
+  cellsize <- c(diff(st_bbox(shape_meters)[c(1, 3)]),
+                diff(st_bbox(shape_meters)[c(2, 4)]))/n
   radius <- sqrt((cellsize[1]/2)^2 + (cellsize[2]/2)^2)
   
-  results <- map_dfr(detroit, search_googlemap, radius)
-  
+  results <- map_dfr(shape, search_googlemap,
+                     query, radius, .progress = TRUE) |>
+    mutate(city = city) |> 
+    distinct()
   return(results)
 }
+
+# Wraps the above
+get_googlemap <- function(){
+  boundaries <- bind_rows(config)
+  all_restaurants <- imap_dfr(config,
+                      get_geography_restaurants, "restaurants", 50)
+  black_owned <- imap_dfr(config,
+                      get_geography_restaurants, "black-owned restaurants", 10) |> 
+    mutate(black_owned = TRUE)
+  all_results <- bind_rows(all_restaurants, black_owned)
+  saveRDS(all_results, "static/all_results.RDS")
+  
+  # Clip to city bounds
+  results_clipped <- all_results |> 
+    st_as_sf(coords = c("lng", "lat")) |> 
+    st_set_crs(st_crs(boundaries)) |> 
+    st_join(boundaries, left = FALSE)
+  
+  # Deduplicate black_owned restaurants which appear in both result sets
+  results_filtered <- results_clipped |> 
+    group_by(pick(everything(), -black_owned)) |> 
+    # Keep if unique (unduplicated) or if Black-owned
+    filter(n() == 1 | black_owned) |> 
+    ungroup() |> 
+    mutate(black_owned = !is.na(black_owned))
+  
+  # splitting by city and black-owned
+  walk(names(config),
+        function(city_name){
+          walk(c(TRUE, FALSE), \(is_black_owned){
+            data_subset <- results_filtered |> 
+              filter(city == city_name, black_owned == is_black_owned)
+            message(nrow(data_subset))
+            filename <- paste0("static/", tolower(city), ifelse(is_black_owned, "_non", ""),
+                               "_black_owned.json")
+            file.remove(filename)
+            st_write(data_subset, filename, driver = "GeoJSON")
+            })
+          })
+
+  return(results_clipped)
+}
+
+results <- get_googlemap()
